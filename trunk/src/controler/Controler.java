7e3314400f17de1a3dcf9f121e8fde484c795123
tristan.bardey@gmail.com <tristan.bardey@gmail.com@6e489abd-8356-3794-0d75-2c1d9dcc6abd>
2014-12-30 21:04:45 +0000

    connection establishment (i think)

--- trunk/src/controler/Controler.java
+++ trunk/src/controler/Controler.java
@@ -4,4 +4,11 @@ public class Controler implements ServletsToControler{
 
 	//ici on met toute l'implémentation des signaux qui sont dans le ServletsToControler
 	
+	
+	public int userIdAndPasswordAregood(String userName, String pwd){
+		return 666;
+	}
+	public boolean isAdmin(int userID){
+		return true;
+	}
 }
--- trunk/src/sopracarpooling/servlets/Logout.java
+++ trunk/src/sopracarpooling/servlets/Logout.java
@@ -1,47 +1,45 @@
 package sopracarpooling.servlets;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
- * Servlet implementation class CreatAccount
+ * Servlet implementation class Logout
  */
-@WebServlet("/CreatAccount")
-public class CreatAccount extends HttpServlet {
+@WebServlet("/Logout")
+public class Logout extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public CreatAccount() {
+    public Logout() {
         super();
         // TODO Auto-generated constructor stub
     }
 
-	/**
+	/**Is called when the user wants to logout from the site, it disconnects the user from the site
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub		
+		HttpSession s = request.getSession();
+		s.invalidate();
+		response.sendRedirect("test");//has to send back to login page. 
+		
 	}
 
-	/**
+	/**Should not be used 
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		
-		//Grabe all the attributes and test if there are not null
-		
-		//call Melinas methode to creat an account 
-		
-		//if everithing went well send the user to the rides page. 
-		
-		
 		
 	}
 
