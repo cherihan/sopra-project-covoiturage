633069264959c18cb9b27d3bfd768dd0887b86a1
dauriac.al@gmail.com <dauriac.al@gmail.com@6e489abd-8356-3794-0d75-2c1d9dcc6abd>
2015-01-03 20:44:41 +0000

    Signal requestUserRides + ajout commentaires

--- src/database/DatabaseInterface.java
+++ src/database/DatabaseInterface.java
@@ -1,13 +1,20 @@
 package database;
 
+import java.util.*;
+
+import model.Ride;
+
 public interface DatabaseInterface {
 
 	//ici on trouve la définitions de tous les signaux que mélina peux envoyer à alex
-	
 	//pas d'implémentation que des définitions
 	
 	//Retourne 0 si pas enregistré, 1 si enregistré, 2 si c'est un admin (-1 si erreur)
 	public int requestUserIsRegistered(String username, String password);
+	
+	//renvoie un tableau contenant tous les trajets correspondant (même code postal et même site sopra)
+	//à ceux de l'utilisateur donné (null si pas de correspondance)
+	public ArrayList<Ride> requestUserRides(String username);
 	
 	
 }
