beaa1f34a2c5d93b640d0b3f4dbc1398d4f8366c
dauriac.al@gmail.com <dauriac.al@gmail.com@6e489abd-8356-3794-0d75-2c1d9dcc6abd>
2015-01-03 20:44:41 +0000

    Signal requestUserRides + ajout commentaires

--- trunk/src/database/DataBaseAccess.java
+++ trunk/src/database/DataBaseAccess.java
@@ -7,12 +7,15 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
+import model.*;
+
 public class DataBaseAccess implements DatabaseInterface{
 	
 	//ici alex implémentes tous les signaux envoyer par Mélina
 	//impléménattion ! 
 	
 	
+	//Connexion à la db
 	private Connection Connexion(){
 		try {    
 		    String url = "jdbc:mysql://localhost/sopra";
@@ -28,38 +31,109 @@ public class DataBaseAccess implements DatabaseInterface{
 		
 	}
 	
+	//fermeture connexion etc.
+	private void Close(ResultSet resultat, Statement statement, Connection connexion){
+		if (resultat != null) {
+			try {
+				resultat.close();
+			} catch (Exception e) {}
+		}
+		if (statement != null) {
+			try {
+				statement.close();
+			} catch (Exception e) {}
+		}
+		if (connexion != null) {
+			try {
+				connexion.close();
+			} catch (Exception e) {}
+		}
+	}
+	
 	public int requestUserIsRegistered(String username, String password){
 		
-		Connection connexion;
-		Statement statement;
-		ResultSet resultat;
+		Connection connexion=null;
+		Statement statement=null;
+		ResultSet resultat=null;
 		int isRegistered;
 		
 		try{
+		//connexion
 		connexion = Connexion();
 		statement = connexion.createStatement();
+		//chercher si l'utilisateur existe
 		resultat = statement.executeQuery(
 			"SELECT COUNT(1) FROM user WHERE nickname="+username+" AND password="+password );
 		if(resultat.getInt(1) == 1){
+			//vérifier si l'utilisateur est admin ou pas
 			resultat = statement.executeQuery(
 				"SELECT nickname,isAdmin FROM user WHERE nickname="+username+" AND password="+password);
 			if(resultat.getInt("isAdmin") == 1){
+				//c'est un admin
 				isRegistered = 2;
 			}else{
+				//c'est un user lambda
 				isRegistered = 1;
 			}
 			
 		}else{
+			//le mec/la gente demoiselle n'existe pas
 			isRegistered = 0;	
 		}
 		return isRegistered;
 		}catch (Exception e){
 			System.err.println("Erreur requête utilisateur enregistré : "+e);
 			return -1;
+		} finally {
+			Close(resultat, statement, connexion);
 		}
-		
-		
 	}
+	
+	public ArrayList<Ride> requestUserRides(String username){
+		Connection connexion=null;
+		Statement statement=null;
+		ResultSet resultat=null;
+		Ride ride;
+		ArrayList<Ride> rides = new ArrayList<Ride>();
+		
+		try{
+			//connexion
+		connexion = Connexion();
+		statement = connexion.createStatement();
+		//sélectionner l'utilisateur puis ses trajets
+		resultat = statement.executeQuery(
+			"SELECT id FROM user WHERE nickname="+username);
+		resultat = statement.executeQuery(
+				"SELECT id,cp,sopra_site,sens FROM rides WHERE id="+resultat.getInt(1));
+		resultat =statement.executeQuery(
+				"SELECT cp,sopra_site,sens FROM rides WHERE cp="+resultat.getInt("cp")+"AND sopra_site="+resultat.getString("sopra_site"));
+		//tant qu'il reste des lignes dans le tableau de résultat
+        while ( resultat.next() ) {
+        	//prendre les valeurs de la ligne
+            int cp = resultat.getInt("cp");
+            String site = resultat.getString( "sopra_site" );
+            boolean sens = false;
+            if (resultat.getInt("sens") == 0){
+            	sens=false;
+            }else if (resultat.getInt("sens") == 1){
+            	sens=true;
+            }            
+            PostCode code = new PostCode(cp);
+            //"créer" un trajet
+            ride = new Ride(code,site,sens);
+            //mettre tous les trajets dans un tableau de trajets
+            rides.add(ride);
+		}
+        Close(resultat, statement, connexion);
+		}catch(Exception e){
+			System.err.println("Erreur requête trajets : "+e);
+			return null;
+		} finally {
+		Close(resultat, statement, connexion);
+		}
+		return rides;
+	}
+	
 	
 	
 	
