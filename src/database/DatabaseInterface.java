59359e0b040f40c425ad9911fc169492b8f9d9ea
dauriac.al@gmail.com <dauriac.al@gmail.com@6e489abd-8356-3794-0d75-2c1d9dcc6abd>
2015-01-04 17:57:26 +0000

    modif des signaux pour coller avec les modifs du model

--- src/database/DatabaseInterface.java
+++ src/database/DatabaseInterface.java
@@ -10,11 +10,11 @@ public interface DatabaseInterface {
 	//pas d'implémentation que des définitions
 	
 	//Retourne 0 si pas enregistré, 1 si enregistré, 2 si c'est un admin (-1 si erreur)
-	public int requestUserIsRegistered(String username, String password);
+	public int requestUserIsRegistered(String mail);
 	
 	//renvoie un tableau contenant tous les trajets correspondant (même code postal et même site sopra)
 	//à ceux de l'utilisateur donné (null si pas de correspondance)
-	public ArrayList<Ride> requestUserRides(String username);
+	public ArrayList<Ride> requestUserRides(String mail);
 	
 	
 }
