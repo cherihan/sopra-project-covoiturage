bf4e7d746edb7b6845eb704257ef63b39b9f5c34
tristan.bardey@gmail.com <tristan.bardey@gmail.com@6e489abd-8356-3794-0d75-2c1d9dcc6abd>
2015-01-06 14:47:14 +0000

    database = model

--- src/model/Admin.java
+++ src/model/Admin.java
@@ -3,7 +3,7 @@ package model;
 public class Admin extends User {
 	
 	public Admin(){
-		super(32,"Sopra","JC","the.police.sting@hotmail.com","peut tout faire");
+		super(32,"Sopra","JC",new EmailAdresse("the.police.sting@hotmail.com"),"peut tout faire", new NumeroTelephone("0834557221"));
 	}
 	
 	
@@ -12,7 +12,7 @@ public class Admin extends User {
 	public Admin(User user){
 		
 		//check avec la database que c'est bien un admin et la c'est parti sinon on nic tout ! 
-		super(32,"Sopra","JC","the.police.sting@hotmail.com","peut tout faire");
+		super(32,"Sopra","JC",new EmailAdresse("the.police.sting@hotmail.com"),"peut tout faire", new NumeroTelephone("0834557221"));
 	}
 
 }
---
+++ src/model/NumeroTelephone.java
@@ -0,0 +1,16 @@
+package model;
+
+
+public class NumeroTelephone {
+	
+	private String telNum;
+	
+	public NumeroTelephone (String telNum) {
+		this.telNum = telNum;
+	}
+	
+	public String ToString () {
+		return this.telNum;
+	}
+
+}
