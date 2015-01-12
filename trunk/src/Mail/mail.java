abe4691fc8f0cf8a1d703b7c4eed3b3d75b64017
melinamanolias@gmail.com <melinamanolias@gmail.com@6e489abd-8356-3794-0d75-2c1d9dcc6abd>
2015-01-12 18:25:20 +0000

    AJOUT MAILS

---
+++ trunk/src/Mail/mail.java
@@ -0,0 +1,56 @@
+/*
+
+package Mail;
+
+import javax.websocket.Session;
+import java.util.Properties;
+import com.sun.corba.se.impl.protocol.giopmsgheaders.Message;
+
+public class mail extends Thread {
+
+	
+	public mail() {
+		
+		 L'adresse IP de votre serveur SMTP 
+		String smtpServer = "";
+
+		/* L'adresse de l'expéditeur 
+		String from = "manolias@etud.insa-toulouse.fr";
+
+		/* L'adresse du destinataire PARAM
+		String to = "";
+
+		/* L'objet du message PARAM 
+		String objet = "";
+
+		/* Le corps du mail 
+		String texte = "Texte du mail";
+
+		Properties props = System.getProperties();
+		props.put("mail.smtp.host", smtpServer);
+		// Session encapsule pour un client donné sa connexion avec le serveur de mails
+		Session session = Session.getDefaultInstance(props, null);
+
+		Création du message
+		Message msg = new MimeMessage(session);
+
+		try {
+		      msg.setFrom(new InternetAddress(from));
+		      msg.setRecipients(Message.RecipientType.TO,InternetAddress.parse(to, false));
+		      msg.setSubject(objet);
+		      msg.setText(texte);
+		      msg.setHeader("X-Mailer", "LOTONtechEmail");
+		      Transport.send(msg);
+		}
+		catch (AddressException e) {
+		      e.printStackTrace();
+		} 
+		catch (MessagingException e) {
+		      e.printStackTrace();
+		} 
+	} 
+}
+*/
+
+
+		
