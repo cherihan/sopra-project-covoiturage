52c579d169155cdb63fdc8b90cf7f7b4037cb9ce
tristan.bardey@gmail.com <tristan.bardey@gmail.com@6e489abd-8356-3794-0d75-2c1d9dcc6abd>
2015-01-13 09:44:05 +0000

    using filters

--- trunk/src/sopracarpooling/servlets/RidesUpdate.java
+++ trunk/src/sopracarpooling/servlets/RidesUpdate.java
@@ -20,7 +20,7 @@ import model.*;
 /**
  * Servlet implementation class RidesUpdate
  */
-@WebServlet("/RidesUpdate")
+@WebServlet("/u/RidesUpdate")
 public class RidesUpdate extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
